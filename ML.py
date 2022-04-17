f05153a09f9be52395c9477ca55fed5a237b9424
ronvic12 <58205103+ronvic12@users.noreply.github.com>
2022-04-16 19:54:09 -0700

    separated values

--- ML.py
+++ ML.py
@@ -13,10 +13,18 @@ def index():
 def result():
    if request.method == 'POST':
       result = request.form
-      print(result) # use result ig and put the data here. 
+      # just to get the values easily
+      readinglevel = request.values.get('readinglevel')
+      BookRating = request.values.get('BookRating')
+      BookName  = request.values.get('BookName')
+      BookPDF = request.values.get('BookPDF')
+
+      print(readinglevel) # use result ig and put the data here. 
+      print(BookRating)
+      print(BookName)
+      print(BookPDF)
+      
       return render_template("result.html",result = result)
-
-
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
